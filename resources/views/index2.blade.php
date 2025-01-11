774e48082e88768d3a61887f36169ceac2c885bc
nourahaies <naiefnoura@gmail.com>
2025-01-11 14:39:41 +0300

    dash

--- resources/views/index2.blade.php
+++ resources/views/index2.blade.php
@@ -45,6 +45,11 @@
         <p>Here you can manage the orders </p>
     </div>
 </div>
+
+<!-- زر Logout -->
+<div class="logout-btn-container">
+    <a href="#" class="logout-btn">Logout</a>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <script src="{{ asset('js/script.js') }}"></script>
 </body>
